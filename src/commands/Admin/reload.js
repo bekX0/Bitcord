57472c66b3ca8dd1bfbc66792400190111f05e77
bekX0 <hikmetberkin@gmail.com>
2023-08-11 21:59:32 +0300

    reload & deploy commands added

---
+++ src/commands/Admin/reload.js
@@ -0,0 +1,65 @@
+const { SlashCommandBuilder } = require("discord.js");
+const fs = require("node:fs");
+
+module.exports = {
+  cooldown: 3,
+  data: new SlashCommandBuilder()
+    .setName("reload")
+    .setDescription("Reloads a command.")
+    .addStringOption((option) =>
+      option
+        .setName("command")
+        .setDescription("The command to reload.")
+        .setRequired(true)
+        .setAutocomplete(true)
+    ),
+  async autocomplete(interaction) {
+    const focusedValue = interaction.options.getFocused();
+    // const choices = interaction.client.commands.map(command => command.data.name)
+    const choices = Array.from(interaction.client.commands.keys());
+    const filtered = choices.filter((choice) =>
+      choice.startsWith(focusedValue)
+    );
+    await interaction.respond(
+      filtered.map((choice) => ({ name: choice, value: choice }))
+    );
+  },
+
+
+  async execute(interaction) {
+    const commandName = interaction.options
+      .getString("command", true)
+      .toLowerCase();
+    const command = interaction.client.commands.get(commandName);
+
+    if (!command) {
+      return interaction.reply(
+        `There is no command with name \`${commandName}\`!`
+      );
+    }
+
+    var filePath;
+    fs.readdirSync("./src/commands").forEach((folder) => {
+      fs.readdirSync(`./src/commands/${folder}`).filter((file) => {
+        if (file === `${commandName}.js`) {
+          filePath = `../${folder}/${file}`;
+        }
+      });
+    });
+    delete require.cache[require.resolve(filePath)];
+
+    try {
+      interaction.client.commands.delete(command.data.name);
+      const newCommand = require(filePath);
+      interaction.client.commands.set(newCommand.data.name, newCommand);
+      await interaction.reply(
+        `Command \`${newCommand.data.name}\` was reloaded!`
+      );
+    } catch (error) {
+      console.error(error);
+      await interaction.reply(
+        `There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``
+      );
+    }
+  },
+};
--- src/commands/General/ping.js
+++ src/commands/General/ping.js
@@ -1,9 +1,9 @@
+const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
-    data:{
-        name:'ping'
-    },
-    execute: () => {
-
-    }
-
-}
+	data: new SlashCommandBuilder()
+		.setName('ping')
+		.setDescription('Replies with Pong!'),
+	async execute(interaction) {
+		await interaction.reply('Pong!');
+	}
+};
